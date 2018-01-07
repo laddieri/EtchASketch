2723555bab8bfe80a8e5bb9f9419d2ad2672223e
laddieri <laddieri@gmail.com>
2018-01-07 11:34:25 -0500

    Fully working Etch-A-Sketch

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,12 +1,50 @@
-const container = document.querySelector('#container');
+const body = document.querySelector('body');
+const resetDiv = document.querySelector('resetButton');
 
-let size = 10;
+function drawContainer () {
+  const container = document.createElement('div');
+  container.setAttribute('id','container');
+  container.style.border="1px solid red";
 
-for (let i = 0; i<size*size;i++) {
-  const div = document.createElement('div');
-  div.style.border = "1px solid black";
-  div.style.height = "10%";
-  div.style.width = "10%";
-  div.style.float="left";
-  container.appendChild(div);
+  return container;
 }
+
+let container = drawContainer();
+body.appendChild(container);
+
+
+function drawGrid (gridSize, containerSize) {
+    let blockSize = containerSize / gridSize;
+    container.style.height = containerSize + "px";
+    container.style.width = containerSize +"px";
+
+  for (let i=0;i<gridSize*gridSize;i++){
+    const drawDiv = document.createElement('div');
+    drawDiv.style.width="" + blockSize +"px";
+    drawDiv.classList.add("drawDiv");
+    drawDiv.style.height= "" + blockSize +"px";
+    container.appendChild(drawDiv);
+
+    let allDrawDivs = document.querySelectorAll('.drawDiv');
+    allDrawDivs.forEach(drawDiv => {
+      drawDiv.addEventListener('mouseover', function(event) {
+        event.target.style.background = '#ff4468';
+      });
+    });
+  }
+}
+
+drawGrid(11, 700);
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', function(e){
+  let choice = prompt("Enter a number between 1-20 to create a new grid to draw on.");
+  if (choice > 20) {
+    choice = prompt("You must enter a number that is less than 20");
+  }
+  const newContainer = drawContainer();
+  body.replaceChild(newContainer,container);
+  container=newContainer;
+
+  drawGrid(choice, 700);
+})
